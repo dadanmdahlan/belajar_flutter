fe37b7ac6638c5d4bde72e69acd4654ea124838c
dadanmdahlan <dadan.muhammad13@gmail.com>
2021-06-03 09:23:36 +0700

    "belajar State full"

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 
 
+
+
 import 'package:flutter/material.dart';
 
 
@@ -20,9 +22,59 @@ class MyApp extends StatelessWidget {
           title: Text('Hello,world!'),
           ),
           body :Center (
-            child:Text('Hello,world!'),
+            child:BiggerText(
+              text:"Hello World",
+            ),
           ),
         ),
       );
+  }
+}
+
+class Heading extends StatelessWidget{
+  final String text;
+  Heading({this.text});
+
+  @override
+  Widget build(BuildContext context){
+    return Text(
+      text,
+      style:TextStyle(
+        fontSize: 24.0,
+        fontWeight: FontWeight.bold,
+      ),
+    );
+  }
+}
+
+class BiggerText extends StatefulWidget {
+  final String text;
+
+  const BiggerText({this.text});
+
+  @override 
+  _BiggerTextState createState() => _BiggerTextState();
+}
+
+class _BiggerTextState extends State<BiggerText> {
+  double _textSize =16.0;
+
+  @override 
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Text(widget.text,style:TextStyle(fontSize: _textSize)),
+        ElevatedButton( 
+          child: Text("Perbesar"),
+          onPressed: (){
+            setState(() {
+              _textSize=32.0;
+              TextStyle(fontWeight: FontWeight.bold,);
+            });
+          },
+        )
+      ],
+    );
   }
 }
